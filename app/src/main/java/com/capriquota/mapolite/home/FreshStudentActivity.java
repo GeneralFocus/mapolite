f34ff0f0b2c20cf7e3bdcfe9ef20bdcf05c63103
IfeanyiOpara <Itzfelz.16@gmail.com>
2020-07-16 12:57:59 +0100

    Worked on UI

--- app/src/main/java/com/capriquota/mapolite/home/FreshStudentActivity.java
+++ app/src/main/java/com/capriquota/mapolite/home/FreshStudentActivity.java
@@ -1,8 +1,11 @@
 package com.capriquota.mapolite.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.capriquota.mapolite.R;
 
@@ -12,5 +15,19 @@ public class FreshStudentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fresh_student);
+
+        Toolbar toolbar = findViewById(R.id.fresh_student_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Fresh Student");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new Intent(FreshStudentActivity.this, HomeActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/capriquota/mapolite/home/LMS.java
+++ app/src/main/java/com/capriquota/mapolite/home/LMS.java
@@ -1,8 +1,11 @@
 package com.capriquota.mapolite.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.capriquota.mapolite.R;
 
@@ -12,5 +15,20 @@ public class LMS extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lms);
+
+        Toolbar toolbar = findViewById(R.id.lms_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("LMS");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new Intent(LMS.this, HomeActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
     }
 }
