6c92bb45b002a2981e585ce809e38fde74ee5549
IfeanyiOpara <Itzfelz.16@gmail.com>
2020-07-16 12:13:31 +0100

    UI completed

---
+++ app/src/main/java/com/capriquota/mapolite/home/updatesActivity.java
@@ -0,0 +1,16 @@
+package com.capriquota.mapolite.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.capriquota.mapolite.R;
+
+public class updatesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updates);
+    }
+}
